3c2b9f4a2fc023bcbab6d70d72ef5bcc2bd00cb3
Andrey <i.andrey.py@gmail.com>
2025-09-17 23:22:17 +0300

    3 глава

--- test_todo.py
+++ test_todo.py
@@ -7,3 +7,22 @@ def test_add_todo(page):
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
     page.get_by_placeholder("What needs to be done?").press("Enter")
+
+def test_form(page):
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    expect(page).to_have_url("https://demo.playwright.dev/todomvc/#/")
+
+    input_field = page.get_by_placeholder('What needs to be done?')
+    expect(input_field).to_be_empty()
+
+    input_field.fill("задача номер 1")
+    input_field.press('Enter')
+    input_field.fill("задача номер 2")
+    input_field.press('Enter')
+    page.pause()
+    todo_item = page.get_by_test_id('todo-item')
+    expect(todo_item).to_have_count(2)
+
+    todo_item.get_by_role('checkbox').first.click()
+    expect(todo_item.first).to_have_class('completed')
+
